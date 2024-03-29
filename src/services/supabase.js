7de0a6ad393e2d180afa351e10d2e541bda5dc17
AbdullahPro2 <zeb16208@gmail.com>
2024-03-29 18:09:03 +0100

    ReactQuery Setup

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://uavptuorjmncdynshyyj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdnB0dW9yam1uY2R5bnNoeXlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxODM0OTEsImV4cCI6MjAyNTc1OTQ5MX0.J9PZCriumDTfxGkpqazzdAALisEeU7f2tV6GWj8ifR0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
